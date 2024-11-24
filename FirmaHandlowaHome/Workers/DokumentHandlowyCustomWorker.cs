20b31ea21ecb3b13aa517c7b032d4cd537110421
Gruntmaster <andrew.horlanov@gmail.com>
2024-11-24 13:46:34 +0100

    Add project files.

---
+++ FirmaHandlowaHome/Workers/DokumentHandlowyCustomWorker.cs
@@ -0,0 +1,26 @@
+﻿using FirmaHandlowaHome.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(DokumentHandlowyCustomWorker),typeof(DokumentHandlowy))]
+
+namespace FirmaHandlowaHome.Workers
+{
+    public class DokumentHandlowyCustomWorker
+    {
+        [Action("Testowy prycisk",
+            CommandStyle = Soneta.Commands.CommandStyle.Red,
+            Icon = ActionIcon.Database,
+            Target = ActionTarget.ToolbarWithText,
+            Mode = ActionMode.SingleSession | ActionMode.OnlyForm | ActionMode.ConfirmFinished)]
+        public void DocumentAction()
+        {
+
+        }
+    }
+}
