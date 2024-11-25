33a1a6a257aab9dbfdcf98dddf7830effc2587e4
Gruntmaster <andrew.horlanov@gmail.com>
2024-11-25 19:42:55 +0100

    Rabat + DaneTechnicznePage

---
+++ FirmaHandlowaHome/Workers/CopyDocumentPositionWorker.cs
@@ -0,0 +1,53 @@
+﻿using FirmaHandlowaHome.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(CopyDocumentPositionWorker), typeof(DokumentHandlowy))]
+
+namespace FirmaHandlowaHome.Workers
+{
+    public class CopyDocumentPositionWorker
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public Context Context { get; set; }
+
+        [Action("Kopiuj pozycję",
+            CommandStyle = Soneta.Commands.CommandStyle.Blue,
+            Icon = ActionIcon.Accept,
+            Target = ActionTarget.ToolbarWithText,
+            Mode = ActionMode.SingleSession | ActionMode.OnlyForm | ActionMode.ConfirmFinished)]
+
+        public void Copy()
+        {
+            INavigatorContext navigatorContext = Context[typeof(INavigatorContext)] as INavigatorContext;
+            PozycjaDokHandlowego pozycja = navigatorContext.FocusedRow as PozycjaDokHandlowego;
+
+            if (pozycja == null)
+            {
+                return;
+            }
+
+            DokumentHandlowy dokumentHandlowy = Context[typeof(DokumentHandlowy)] as DokumentHandlowy;
+
+            using(ITransaction transaction = Session.Logout(true))
+            {
+                PozycjaDokHandlowego pozycjaKopia = new PozycjaDokHandlowego(dokumentHandlowy);
+                Session.AddRow(pozycjaKopia);
+                pozycjaKopia.Towar = pozycja.Towar;
+                pozycjaKopia.Cena = pozycja.Cena;
+                pozycjaKopia.Ilosc = pozycja.Ilosc;
+                pozycjaKopia.Rabat = pozycja.Rabat;
+
+                transaction.CommitUI();
+            }
+        }
+    }
+}
---
+++ FirmaHandlowaHome/Workers/DocumentRabatPositionWorker.cs
@@ -0,0 +1,49 @@
+﻿using FirmaHandlowaHome.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(DocumentRabatPositionWorker), typeof(DokumentHandlowy))]
+namespace FirmaHandlowaHome.Workers
+{
+    public class DocumentRabatPositionWorker
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public DokumentHandlowy DokumentHandlowy { get; set; }
+
+        [Action("Ustaw rabat",
+          CommandStyle = Soneta.Commands.CommandStyle.Green,
+          Icon = ActionIcon.Book,
+          Target = ActionTarget.ToolbarWithText,
+          Mode = ActionMode.SingleSession | ActionMode.OnlyForm | ActionMode.ConfirmFinished)]
+
+        public string DocumentRabatAction()
+        {
+            using (ITransaction transaction = Session.Logout(true))
+            {
+               foreach(PozycjaDokHandlowego pozycja in DokumentHandlowy.Pozycje) 
+                {
+                    pozycja.Cena = pozycja.Cena * 1.2;
+                }
+                transaction.CommitUI();
+            }
+            return "Zwiększono cenę wszystkich pozycji o 20%";
+        }
+
+        public bool IsEnabledDocumentRabatAction()
+        {
+            if (DokumentHandlowy.Bufor)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
