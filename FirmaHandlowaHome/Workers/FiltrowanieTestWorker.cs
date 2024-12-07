bb5bdfd78f2bfc8b267a1848cfe5d9b16464ca2e
Gruntmaster <andrew.horlanov@gmail.com>
2024-12-07 14:47:19 +0100

    FiltrowanieTestWorker

---
+++ FirmaHandlowaHome/Workers/FiltrowanieTestWorker.cs
@@ -0,0 +1,28 @@
+﻿using FirmaHandlowaHome.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(FiltrowanieTestWorker), typeof(DokumentHandlowy))]
+
+namespace FirmaHandlowaHome.Workers
+{
+    public class FiltrowanieTestWorker
+    {
+        [Context]
+        public Session Session { get; set; }    
+
+        [Action("Filtrowanie test",
+           Icon = ActionIcon.Wizard,
+           Target = ActionTarget.ToolbarWithText,
+           Mode = ActionMode.SingleSession | ActionMode.ConfirmFinished)]
+        public void DocumentAction()
+        {
+
+        }
+    }
+}
