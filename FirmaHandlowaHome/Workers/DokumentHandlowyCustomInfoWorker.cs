4dcc35ba5dab410e0ec00cec65d3882fe43c06f1
Gruntmaster <andrew.horlanov@gmail.com>
2024-12-07 13:11:03 +0100

    Planowana data dostawy

--- FirmaHandlowaHome/Workers/DokumentHandlowyCustomInfoWorker.cs
+++ FirmaHandlowaHome/Workers/DokumentHandlowyCustomInfoWorker.cs
@@ -23,12 +23,29 @@ namespace FirmaHandlowaHome.Workers
             get
             {
                 string result = string.Empty;
-                foreach(PozycjaDokHandlowego pozycja in DokumentHandlowy.Pozycje)
+                foreach (PozycjaDokHandlowego pozycja in DokumentHandlowy.Pozycje)
                 {
                     result += pozycja.Towar.Kod + "; ";
                 }
 
                 return result;
+            }
+        }
+
+        [Caption("Planowana data dostawy")]
+        public Date PlanowanaDataDostawy
+        {
+            get
+            {
+               return DokumentHandlowy.Features.GetDate("PlanowanaDataDostawy");
+            }
+            set
+            {
+                if (value < Date.Today)
+                {
+                    throw new Exception("Nie można wprowadzić daty przeszłej!");
+                }
+                DokumentHandlowy.Features["PlanowanaDataDostawy"] = value;
             }
         }
     }
