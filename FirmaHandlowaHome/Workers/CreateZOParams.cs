4a1e6e567c7bd5cb0bdd51e06bc51d1923844d44
Gruntmaster <andrew.horlanov@gmail.com>
2024-12-15 12:21:07 +0100

    CreateZOWorker + ParamsZO

---
+++ FirmaHandlowaHome/Workers/CreateZOParams.cs
@@ -0,0 +1,60 @@
+﻿using Soneta.Business;
+using Soneta.CRM;
+using Soneta.Magazyny;
+using Soneta.Towary;
+using Soneta.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirmaHandlowaHome.Workers
+{
+    public class CreateZOParams : ContextBase
+    {
+        private Kontrahent _kontrahent;
+        private Magazyn _magazyn;
+        private Towar _towar;
+        public CreateZOParams(Context context) : base(context) 
+        { 
+        
+        }
+
+        [Caption("Kontrahent")]
+        [Required]
+        public Kontrahent Kontrahent
+        {
+            get => _kontrahent;
+            set
+            {
+                _kontrahent = value;
+                OnChanged();
+            }
+        }
+
+        [Caption("Magazyn")]
+        [Required]
+        public Magazyn Magazyn
+        {
+            get => _magazyn;
+            set
+            {
+                _magazyn = value;
+                OnChanged();
+            }
+        }
+
+        [Caption("Towar")]
+        [Required]
+        public Towar Towar
+        {
+            get => _towar;
+            set
+            {
+                _towar = value;
+                OnChanged();
+            }
+        }
+    }
+}
---
+++ FirmaHandlowaHome/Workers/CreateZOWorker.cs
@@ -0,0 +1,57 @@
+﻿using FirmaHandlowaHome.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(CreateZOWorker),typeof(DokHandlowe))]
+
+namespace FirmaHandlowaHome.Workers
+{
+    public class CreateZOWorker
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public DokumentHandlowy DokumentHandlowy { get; set; }
+
+        [Context]
+        public CreateZOParams Params { get; set; }
+
+        [Action("Zrób nowy ZO",
+          CommandStyle = Soneta.Commands.CommandStyle.Green,
+          Icon = ActionIcon.Book,
+          Target = ActionTarget.ToolbarWithText,
+          Mode = ActionMode.SingleSession | ActionMode.OnlyForm | ActionMode.ConfirmFinished)]
+
+        public object DocumentNewZOAction()
+        {
+            DefDokHandlowego defDokHandlowego = Session.GetHandel().DefDokHandlowych.WgSymbolu["ZO"];
+
+            DokumentHandlowy dokumentHandlowy;
+            using (ITransaction transaction = Session.Logout(true))
+            {
+                dokumentHandlowy = new DokumentHandlowy();
+                dokumentHandlowy.Definicja = defDokHandlowego;
+                dokumentHandlowy.Magazyn = Params.Magazyn;
+                Session.AddRow(dokumentHandlowy);
+
+                dokumentHandlowy.Kontrahent = Params.Kontrahent;
+
+                PozycjaDokHandlowego pozycja = new PozycjaDokHandlowego(dokumentHandlowy);
+                Session.AddRow(pozycja);
+                pozycja.Towar = Params.Towar;
+                pozycja.Ilosc = new Soneta.Towary.Quantity(10, Params.Towar.Jednostka.Kod);
+
+                transaction.CommitUI();
+            }
+            return dokumentHandlowy;
+        }
+
+    }
+}
+
