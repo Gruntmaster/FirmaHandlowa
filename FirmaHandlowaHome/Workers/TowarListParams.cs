0d471b9e755f9e84be857a3b656da0239894deb1
Gruntmaster <andrew.horlanov@gmail.com>
2024-12-16 17:20:23 +0100

    TowarListWorker + TowarListParams

---
+++ FirmaHandlowaHome/Workers/TowarListParams.cs
@@ -0,0 +1,76 @@
+﻿using Soneta.Business;
+using Soneta.CRM;
+using Soneta.Handel;
+using Soneta.Magazyny;
+using Soneta.Tools;
+using Soneta.Types;
+
+namespace FirmaHandlowa.Workers
+{
+    public class TowarListParams : ContextBase
+    {
+        private DefDokHandlowego _definicja;
+        private Magazyn _magazyn;
+        private Kontrahent _kontrahent;
+
+        public TowarListParams(Context context) : base(context)
+        {
+            _definicja = Session.GetHandel().DefDokHandlowych.WgSymbolu["ZO"];
+        }
+
+        [Caption("Definicja dokumentu")]
+        [Required]
+        [Priority(10)]
+        [DefaultWidth(20)]
+        public DefDokHandlowego Definicja
+        {
+            get => _definicja;
+            set
+            {
+                _definicja = value;
+                OnChanged();
+            }
+        }
+
+
+        [Caption("Kontrahent")]
+        [Required]
+        [Priority(20)]
+        [DefaultWidth(20)]
+        public Kontrahent Kontrahent
+        {
+            get => _kontrahent;
+            set
+            {
+                _kontrahent = value;
+                OnChanged();
+
+            }
+        }
+
+        public bool IsVisibleKontrahent()
+        {
+            return _definicja != null;
+        }
+
+
+        [Caption("Magazyn")]
+        [Required]
+        [Priority(30)]
+        [DefaultWidth(20)]
+        public Magazyn Magazyn
+        {
+            get => _magazyn;
+            set
+            {
+                _magazyn = value;
+                OnChanged();
+            }
+        }
+
+        public bool IsReadOnlyMagazyn()
+        {
+            return _definicja == null;
+        }
+    }
+}
---
+++ FirmaHandlowaHome/Workers/TowarListWorker.cs
@@ -0,0 +1,70 @@
+﻿using FirmaHandlowa.Workers;
+using Soneta.Business;
+using Soneta.Handel;
+using Soneta.KadryPlace.Kadry.Workers;
+using Soneta.Towary;
+using Soneta.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: Worker(typeof(TowarListWorker), typeof(Towary))]
+
+namespace FirmaHandlowa.Workers
+{
+    public class TowarListWorker
+    {
+        [Context]
+        public Session Session { get; set; }
+
+        [Context]
+        public Context Context { get; set; }
+
+        [Context]
+        public TowarListParams Params { get; set; }
+
+        [Context]
+        public Towar[] Towary { get; set; }
+
+        [Action("Zrób nowy ZO",
+            CommandStyle = Soneta.Commands.CommandStyle.Blue,
+            Icon = ActionIcon.Accept,
+            Target = ActionTarget.ToolbarWithText,
+            Mode = ActionMode.SingleSession | ActionMode.ConfirmFinished)]
+
+        public object TowarListAction()
+        {
+
+            // Pobieranie zaznaczonych wierszy z INavigatorContext
+
+            //INavigatorContext navigatorContext = Context[typeof(INavigatorContext), false] as INavigatorContext;
+            //Towar[] selectedTowary = navigatorContext.SelectedRows as Towar[];
+
+
+            DokumentHandlowy dokumentHandlowy;
+            using (ITransaction transaction = Session.Logout(true))
+            {
+                dokumentHandlowy = new DokumentHandlowy();
+                dokumentHandlowy.Definicja = Session.GetHandel().DefDokHandlowych.WgSymbolu["OO"];
+                Session.AddRow(dokumentHandlowy);
+
+                dokumentHandlowy.Magazyn = Params.Magazyn;
+                dokumentHandlowy.Kontrahent = Params.Kontrahent;
+
+                foreach (Towar towar in Towary)
+                {
+                    PozycjaDokHandlowego pozycja = new PozycjaDokHandlowego(dokumentHandlowy);
+                    Session.AddRow(pozycja);
+                    pozycja.Towar = towar;
+                    pozycja.Ilosc = new Quantity(1, towar.Jednostka.Kod);
+                    pozycja.Cena = new Currency(12m, "PLN");
+                }
+
+                transaction.CommitUI();
+            }
+            return dokumentHandlowy;
+        }
+    }
+}
